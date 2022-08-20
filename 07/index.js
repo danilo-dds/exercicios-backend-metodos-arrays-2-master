55384309a2bab8290d181449f63c9126c5b71c85
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-08-20 13:57:44 -0300

    coreção do exercicio 07

--- 07/index.js
+++ 07/index.js
@@ -1,14 +1,15 @@
-const enderecos = [
-    { cep: 00111222, rua: "Rua dos Artistas" },
-    { cep: 00111333, rua: "Rua Augusta" },
-    { cep: 00222444, rua: "Avenida Paralela" },
-    { cep: 11222333, rua: "Rua Carlos Gomes" },
-];
+const buscarEdereco = (cep) => {
+    const enderecos = [
+        { cep: 00111222, rua: "Rua dos Artistas" },
+        { cep: 00111333, rua: "Rua Augusta" },
+        { cep: 00222444, rua: "Avenida Paralela" },
+        { cep: 11222333, rua: "Rua Carlos Gomes" },
+    ];
 
+    const enderecoEncotrado = enderecos.find((endereco) => {
+        return endereco.cep === cep;
+    });
+    console.log(enderecoEncotrado.rua);
+};
 
-const localizarEndereco = enderecos.find((endereco) => {
-    const cep = 00222444;
-    return endereco.cep === cep;
-});
-
-console.log(localizarEndereco.rua);
+buscarEdereco(00222444);
